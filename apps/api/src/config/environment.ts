9c193d6ab5b9bdf2a9ecb9b824d863d1df0d5f74
Jesus Diaz <jadiaz.inf@gmail.com>
2026-01-20 19:42:41 -0400

    nothing to commiit

--- apps/api/src/config/environment.ts
+++ apps/api/src/config/environment.ts
@@ -1,4 +1,4 @@
-// environment configuration and validation using zod
+// environment configuration and validation using zod.
 import { z, treeifyError } from 'zod'
 import logger from '@utils/logger'
 
